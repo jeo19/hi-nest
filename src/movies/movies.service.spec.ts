93ace0305651996de285cf89306e63a1fcb8d2e3
jeo19 <terrakkk@outlook.com>
2020-10-03 10:52:31 +0800

    test:Add the testing code which to check the error message is equal

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -39,6 +39,7 @@ describe('MoviesService', () => {
         service.getOne(999);
       } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
       }
     });
   });
