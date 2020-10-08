accd76573fd0efdff25ec113c498ff4fc49b264e
jeo19 <terrakkk@outlook.com>
2020-10-08 11:14:07 +0800

    test:Add the GET('/movies/:id') testing code and the pipe for the type transform

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -28,4 +28,40 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Welcome to my Movie API');
   });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Tenet',
+          year: 2020,
+          genres: ['fantasic', 'history'],
+        })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+        .delete('/movies')
+        .expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
+  });
 });
